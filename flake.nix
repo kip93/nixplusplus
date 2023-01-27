2ec6ffe42002f0d4a4279959388cf69fcdfb9a4f
Leandro Emmanuel Reina Kiperman <leandro@kip93.net>
2023-01-28 00:07:08 +0100

    Update inputs

--- flake.nix
+++ flake.nix
@@ -6,6 +6,14 @@
       url = "git+https://github.com/ryantm/agenix?ref=main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    fenix = {
+      url = "git+https://github.com/nix-community/fenix?ref=main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-compat = {
+      url = "git+https://github.com/edolstra/flake-compat?ref=master";
+      flake = false;
+    };
     flake-utils = {
       url = "git+https://github.com/numtide/flake-utils?ref=master";
     };
@@ -31,12 +39,11 @@
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    naersk = {
+      url = "git+https://github.com/nix-community/naersk?ref=master";
+    };
     nix = {
       url = "git+https://github.com/NixOS/nix?ref=latest-release";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-bundle = {
-      url = "git+https://github.com/matthewbauer/nix-bundle?ref=master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-serve = {
@@ -48,6 +55,18 @@
     };
     nixpkgs = {
       url = "git+https://github.com/NixOS/nixpkgs?ref=nixos-22.11";
+    };
+    nixpkgs-fmt = {
+      url = "git+https://github.com/nix-community/nixpkgs-fmt?ref=master";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.fenix.follows = "fenix";
+    };
+    nixpkgs-lint = {
+      url = "git+https://github.com/nix-community/nixpkgs-lint?ref=master";
+      inputs.naersk.follows = "naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "flake-utils";
+      inputs.flake-compat.follows = "flake-compat";
     };
     nur = {
       url = "git+https://github.com/nix-community/NUR?ref=master";
