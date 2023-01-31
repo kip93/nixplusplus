4861f3e2d6dc0f67a57a8bcaa04d579341a82246
Leandro Emmanuel Reina Kiperman <leandro@kip93.net>
2023-01-31 23:27:34 +0100

    Set up common package metadata

--- packages/default.nix
+++ packages/default.nix
@@ -5,7 +5,21 @@ builtins.listToAttrs
       name = system;
       value = builtins.listToAttrs
         (builtins.map
-          (name: { inherit name; value = import "${./.}/${name}" (inputs // { inherit system; }); })
+          (name: {
+            inherit name;
+            value = (import "${./.}/${name}" (inputs // { inherit system; })).overrideAttrs (super: {
+              meta = self.lib.recursiveUpdate super.meta {
+                homepage = "ssh://git.kip93.net/nix++";
+                maintainers = [
+                  {
+                    name = "Leandro Emmanuel Reina Kiperman";
+                    email = "leandro@kip93.net";
+                    github = "kip93";
+                  }
+                ];
+              };
+            });
+          })
           (builtins.filter
             (name: builtins.pathExists "${./.}/${name}/default.nix")
             (builtins.attrNames (builtins.readDir ./.))
