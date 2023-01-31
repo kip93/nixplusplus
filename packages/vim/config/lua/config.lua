8625b31d03a156a96144a1995e3b1ce98ce0723d
Leandro Emmanuel Reina Kiperman <leandro@kip93.net>
2023-01-31 23:36:22 +0100

    Update vim setup

--- packages/vim/config/lua/config.lua
+++ packages/vim/config/lua/config.lua
@@ -1,3 +1,10 @@
+-- -- Enable verbose logs
+-- vim.cmd [[
+--   autocmd VimEnter * set verbosefile = ~/.cache/nvim.log
+--   autocmd VimEnter * set verbose = 15
+-- ]]
+
+-- Default configs
 vim.o.autoindent = true
 vim.o.backspace = "indent,eol,start"
 vim.o.cindent = true
@@ -20,49 +27,94 @@ vim.opt.mouse = "a"
 vim.o.background = "dark"
 vim.o.cursorline = true
 vim.o.list = true
-vim.o.listchars = "eol:¬,space: ,lead:·,trail:·,nbsp:◇,tab:--→,extends:▸,precedes:◂"
+vim.o.listchars =
+  "eol:¬,space: ,lead:·,trail:·,nbsp:◇,tab:--→,extends:▸,precedes:◂"
 vim.o.number = true
 vim.o.numberwidth = 3
 vim.o.relativenumber = true
 vim.o.signcolumn = "no"
 vim.o.wrap = true
 
+-- Theme
 vim.cmd [[
   colorscheme jellybeans
 ]]
 
-require('gitsigns').setup { numhl = true }
-
+-- Icons
 vim.cmd [[
-  let g:airline_theme='jellybeans'
+  let g:webdevicons_enable_airline_statusline = 1
+]]
+
+-- Git
+require('gitsigns').setup { numhl = true }
+vim.cmd [[
+  autocmd BufEnter * silent! Gcd
+]]
+
+-- Status
+vim.cmd [[
+  let g:airline_theme = 'jellybeans'
   let g:airline_mode_map = {
   \   '__'    : '-',
   \   'c'     : 'C',
   \   'i'     : 'I',
   \   'ic'    : 'I',
   \   'ix'    : 'I',
-  \   'n'     : 'N',
   \   'multi' : 'M',
+  \   'n'     : 'N',
   \   'ni'    : 'N',
   \   'no'    : 'N',
   \   'R'     : 'R',
   \   'Rv'    : 'R',
   \   's'     : 'S',
   \   'S'     : 'S',
-  \   ''     : 'S',
   \   't'     : 'T',
   \   'v'     : 'V',
   \   'V'     : 'V',
+  \   ''     : 'S',
   \   ''     : 'V',
   \ }
   let g:airline_section_a = airline#section#create(['mode'])
   let g:airline_section_b = airline#section#create(['%f'])
-  let g:airline_section_c = airline#section#create(['ffenc','%r:%B'])
+  let g:airline_section_c = airline#section#create(['ffenc', '%r:%B'])
   let g:airline_section_x = airline#section#create(['filetype'])
-  let g:airline_section_y = airline#section#create(['%{get(b:,"gitsigns_head","")}','(','%{get(b:,"gitsigns_status","")}',')'])
+  let g:airline_section_y = airline#section#create([
+  \   '%{get(b:,"gitsigns_head","")}',
+  \   '(',
+  \   '%{get(b:,"gitsigns_status","")}',
+  \   ')'
+  \ ])
   let g:airline_section_z = airline#section#create(['%c:%l/%L'])
 ]]
 
+-- Whitespace
+vim.cmd [[
+  let g:airline#extensions#whitespace#symbol = ''
+  let g:better_whitespace_enabled = 0
+  let g:strip_whitespace_on_save = 1
+  let g:strip_whitespace_confirm = 0
+  let g:strip_whitelines_at_eof = 1
+]]
+
+-- NERD tree
+vim.cmd [[
+  autocmd BufEnter * if winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() |
+  \   quit |
+  \ endif
+
+  let g:NERDTreeGitStatusIndicatorMapCustom = {
+  \   'Modified'  :'~',
+  \   'Staged'    :'+',
+  \   'Untracked' :'*',
+  \   'Renamed'   :'>',
+  \   'Unmerged'  :'!',
+  \   'Deleted'   :'-',
+  \   'Dirty'     :'~',
+  \   'Unknown'   :'?',
+  \ }
+]]
+
+-- Startify
 vim.cmd [[
   let g:startify_custom_header = [
   \   '   #####################################',
@@ -80,6 +132,10 @@ vim.cmd [[
   \ ]
 
   let g:startify_list_order = [ 'bookmarks', 'dir' ]
+
+  function! StartifyEntryFormat()
+    return 'WebDevIconsGetFileTypeSymbol(absolute_path)."  ".entry_path'
+  endfunction
 ]]
 
 pcall(vim.cmd, [[
@@ -88,3 +144,17 @@ pcall(vim.cmd, [[
   \   '{split(v:val)[0]:split(v:val)[1]}'
   \ )
 ]])
+
+-- Startup
+vim.cmd [[
+  autocmd StdinReadPre * let s:std_in = 1
+  autocmd VimEnter * if !exists('s:std_in') |
+  \ NERDTree | wincmd p | endif
+  autocmd VimEnter * if !exists('s:std_in') && (argc() == 0 || isdirectory(argv()[0])) |
+  \ Startify | endif
+]]
+
+-- Others
+vim.cmd [[
+  nnoremap q <Nop>
+]]
--- packages/vim/default.nix
+++ packages/vim/default.nix
@@ -1,15 +1,34 @@
 { nixpkgs, system, ... } @ args:
-with nixpkgs.legacyPackages.${system}; neovim.override {
+with nixpkgs.legacyPackages.${system};
+(neovim.override {
   viAlias = true;
   vimAlias = true;
+
+  withPython3 = false;
+  withNodeJs = false;
+  withRuby = false;
+
   configure = {
     packages.default = with vimPlugins; {
       start = [
         gitsigns-nvim
         jellybeans-vim
+        nerdtree
+        nerdtree-git-plugin
+        (pkgs.vimUtils.buildVimPlugin {
+          name = "vim-nerdtree-syntax-highlight";
+          src = pkgs.fetchFromGitHub {
+            owner = "johnstef99";
+            repo = "vim-nerdtree-syntax-highlight";
+            rev = "0c495b4ec3776946d4b6a9f08c0e48d683af4add";
+            sha256 = "aH3fdAQQjLVth0rYGnqGIGxRZgSPkmpeUfAwVg8feWY=";
+          };
+        })
         unite-vim
         vim-airline
         vim-airline-themes
+        vim-better-whitespace
+        vim-devicons
         vim-fugitive
         vim-nix
         vim-startify
@@ -19,4 +38,11 @@ with nixpkgs.legacyPackages.${system}; neovim.override {
     };
     customRC = "lua require('config')";
   };
-}
+}).overrideAttrs (super: {
+  name = "neovim-nix++";
+  meta = lib.recursiveUpdate super.meta {
+    description = "Neovim, with some pizzazz!";
+    longDescription = null;
+    mainProgram = "nvim";
+  };
+})
