4aaf1fa1a21de1cb9e60d706cdb40399f4b5074d
Leandro Emmanuel Reina Kiperman <leandro@kip93.net>
2023-04-11 00:20:33 +0200

    Remove yet unused flake inputs

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
       url = "git+https://github.com/ryantm/agenix?ref=main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-compat = {
-      url = "git+https://github.com/edolstra/flake-compat?ref=master";
-      flake = false;
-    };
+    # flake-compat = {
+    #   url = "git+https://github.com/edolstra/flake-compat?ref=master";
+    #   flake = false;
+    # };
     flake-utils = {
       url = "git+https://github.com/numtide/flake-utils?ref=main";
     };
@@ -22,32 +22,32 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.utils.follows = "flake-utils";
     };
-    hydra = {
-      url = "git+https://github.com/NixOS/hydra?ref=master";
-      inputs.nix.follows = "nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    impermanence = {
-      url = "git+https://github.com/nix-community/impermanence?ref=master";
-    };
-    microvm-nix = {
-      url = "git+https://github.com/astro/microvm.nix?ref=main";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix = {
-      url = "git+https://github.com/NixOS/nix?ref=latest-release";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # hydra = {
+    #   url = "git+https://github.com/NixOS/hydra?ref=master";
+    #   inputs.nix.follows = "nix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # impermanence = {
+    #   url = "git+https://github.com/nix-community/impermanence?ref=master";
+    # };
+    # microvm-nix = {
+    #   url = "git+https://github.com/astro/microvm.nix?ref=main";
+    #   inputs.flake-utils.follows = "flake-utils";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # nix = {
+    #   url = "git+https://github.com/NixOS/nix?ref=latest-release"; # TODO: 2.13?
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     nixos-hardware = {
       url = "git+https://github.com/NixOS/nixos-hardware?ref=master";
     };
     nixpkgs = {
       url = "git+https://github.com/NixOS/nixpkgs?ref=nixos-unstable"; # TODO: nixos-23.05
     };
-    nur = {
-      url = "git+https://github.com/nix-community/NUR?ref=master";
-    };
+    # nur = {
+    #   url = "git+https://github.com/nix-community/NUR?ref=master";
+    # };
   };
 
   outputs = { ... } @ inputs: {
