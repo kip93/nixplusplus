0c9a839c042d6016392a7de0df908d95e3978767
Leandro Emmanuel Reina Kiperman <leandro@kip93.net>
2023-05-19 18:19:52 +0200

    Remove unused inputs

--- overlays/vimPlugins/default.nix
+++ overlays/vimPlugins/default.nix
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-{ nixpkgs, self, ... } @ inputs:
+{ ... } @ inputs:
 final: prev: with final; {
   vimPlugins = prev.vimPlugins // builtins.listToAttrs (builtins.map
     (pluginConfig: {
