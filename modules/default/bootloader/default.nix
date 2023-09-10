b4371a8a8631b3b41a0d54a0722445e7aa297eb0
Leandro Emmanuel Reina Kiperman <leandro@kip93.net>
2023-09-10 22:45:21 +0200

    Set default bootloader as systemd

---
+++ modules/default/bootloader/default.nix
@@ -0,0 +1,28 @@
+# This file is part of Nix++.
+# Copyright (C) 2023 Leandro Emmanuel Reina Kiperman.
+#
+# Nix++ is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Nix++ is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+{ ... } @ inputs:
+{ lib, ... }:
+{
+  config.boot.loader = {
+    timeout = lib.mkDefault 2;
+    grub.enable = lib.mkDefault false;
+    systemd-boot = {
+      enable = lib.mkDefault true;
+      editor = false;
+      consoleMode = "auto";
+    };
+  };
+}
