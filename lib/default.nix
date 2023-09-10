55adaa59f146e788433150332c2f66663134af50
Leandro Emmanuel Reina Kiperman <leandro@kip93.net>
2023-09-10 21:43:13 +0200

    Fix format

--- lib/default.nix
+++ lib/default.nix
@@ -16,9 +16,7 @@
 { nixpkgs, ... } @ inputs:
 # Cannot use here the nice `import.asLib'` function, since this here is where it
 # is made available.
-builtins.foldl'
-  nixpkgs.lib.recursiveUpdate
-  { }
+builtins.foldl' nixpkgs.lib.recursiveUpdate { }
   (builtins.map
     (name: import (./. + "/${name}") inputs)
     (builtins.filter
