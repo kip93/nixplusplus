c7adc1d804e3a5dfb4272b8602e4500ca18812d7
Leandro Emmanuel Reina Kiperman <leandro@kip93.net>
2023-09-11 17:07:15 +0200

    Remove 1 level of depth in secrets module

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -16,74 +16,73 @@
 { agenix, ... } @ inputs:
 { config, lib, ... }:
 let
-  cfg = config.nixplusplus.${builtins.baseNameOf ./.};
+  secrets = config.nixplusplus.${builtins.baseNameOf ./.};
+  key = config.nixplusplus."${builtins.baseNameOf ./.}_key";
 
 in
 {
   imports = [ agenix.nixosModules.age ];
 
-  options.nixplusplus.${builtins.baseNameOf ./.} = with lib; {
-    key = mkOption {
-      type = types.path;
-      description = mdDoc ''
-        Path to SSH key to be used to decrypt secrets.
-      '';
-    };
-    secrets = mkOption {
-      type = types.attrsOf (types.submodule ({ ... } @ secret: {
-        options = {
-          file = mkOption {
-            type = types.path;
-            description = mdDoc ''
-              A path to an encrypted secret.
-            '';
-          };
-          path = mkOption {
-            type = types.path;
-            readOnly = true;
-            description = mdDoc ''
-              The path where the secret has been decrypted.
-            '';
-            apply = _:
-              config.age.secrets.${secret.config._module.args.name}.path
-            ;
-          };
-          mode = mkOption {
-            type = types.strMatching "[0-7]{3,4}";
-            description = mdDoc ''
-              Permissions of the decrypted secret, in octal format.
-            '';
-            default = "0400";
-          };
-          owner = mkOption {
-            type = types.nonEmptyStr;
-            description = mdDoc ''
-              User that will own the decrypted secret.
-            '';
-            default = "0";
-          };
-          group = mkOption {
-            type = types.nonEmptyStr;
-            description = mdDoc ''
-              Group that will own the decrypted secret.
-            '';
-            default = "0";
-          };
+  options.nixplusplus."${builtins.baseNameOf ./.}_key" = with lib; mkOption {
+    type = types.path;
+    description = mdDoc ''
+      Path to SSH key to be used to decrypt secrets.
+    '';
+  };
+  options.nixplusplus.${builtins.baseNameOf ./.} = with lib; mkOption {
+    type = types.attrsOf (types.submodule ({ ... } @ secret: {
+      options = {
+        file = mkOption {
+          type = types.path;
+          description = mdDoc ''
+            A path to an encrypted secret.
+          '';
         };
-      }));
-      description = mdDoc ''
-        Secrets to be decrypted.
-      '';
-      default = { };
-    };
+        path = mkOption {
+          type = types.path;
+          readOnly = true;
+          description = mdDoc ''
+            The path where the secret has been decrypted.
+          '';
+          apply = _:
+            config.age.secrets.${secret.config._module.args.name}.path
+          ;
+        };
+        mode = mkOption {
+          type = types.strMatching "[0-7]{3,4}";
+          description = mdDoc ''
+            Permissions of the decrypted secret, in octal format.
+          '';
+          default = "0400";
+        };
+        owner = mkOption {
+          type = types.nonEmptyStr;
+          description = mdDoc ''
+            User that will own the decrypted secret.
+          '';
+          default = "0";
+        };
+        group = mkOption {
+          type = types.nonEmptyStr;
+          description = mdDoc ''
+            Group that will own the decrypted secret.
+          '';
+          default = "0";
+        };
+      };
+    }));
+    description = mdDoc ''
+      Secrets to be decrypted.
+    '';
+    default = { };
   };
 
   config = {
     # Map to agenix configurations.
-    age.identityPaths = [ cfg.key ];
+    age.identityPaths = [ key ];
     age.secrets = builtins.mapAttrs
       (_: secret: builtins.removeAttrs secret [ "path" ])
-      cfg.secrets
+      secrets
     ;
   };
 }
