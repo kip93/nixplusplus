395b71c7acc5dea068489fa0dca1480e0304cdb5
Leandro Emmanuel Reina Kiperman <leandro@kip93.net>
2023-12-03 13:16:04 +0100

    Add RegEx for validating IPs

--- checks/lib.functions.strings/default.nix
+++ checks/lib.functions.strings/default.nix
@@ -14,6 +14,51 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 { pkgs, self, ... } @ _args:
+let
+  validIPv4s = [
+    "255.255.255.255"
+    "192.168.1.1"
+    "127.0.0.1"
+    "0.0.0.0"
+    "1.1.1.1"
+    "1.1.1"
+    "1.1"
+  ];
+  validIPv6s = [
+    "0000:1111:2222:3333:4444:5555:192.168.1.1"
+    "0000:1111:2222:3333:4444:5555:6666:7777"
+    "2001:db8:c0a8:101::"
+    "::ffff:c0a8:101"
+    "f:f:f:f:f:f:f:f"
+    "::ABCD:EF"
+    "::1"
+    "::"
+  ];
+  invalidIPv4s = [
+    "256.256.256.256"
+    "1.1.1.1.1"
+    "1.0.0.0."
+    "01.0.0.0"
+    " 1.0.0.0"
+    "1.0.0.0 "
+    "X.X.X.X"
+    "1..1"
+    "1"
+  ];
+  invalidIPv6s = [
+    "0000:1111:2222:3333:4444:5555:6666:7777:8888"
+    "0000:1111:2222:3333:4444:5555:1.1"
+    "::00000"
+    "::1::1"
+    "::1.1"
+    ":::1"
+    " ::1"
+    "::1 "
+    ":::"
+    "::G"
+  ];
+
+in
 pkgs.testers.nixTest {
   name = builtins.baseNameOf ./.;
   checks = {
@@ -86,6 +131,36 @@ pkgs.testers.nixTest {
     setWithOutPath = {
       expr = self.lib.strings.toString { outPath = "/foo"; };
       expected = "/foo";
+    };
+
+    validIPv4s = {
+      expr = builtins.all self.lib.strings.isValidIPv4 validIPv4s;
+      expected = true;
+    };
+
+    validIPv6s = {
+      expr = builtins.all self.lib.strings.isValidIPv6 validIPv6s;
+      expected = true;
+    };
+
+    validIPs = {
+      expr = builtins.all self.lib.strings.isValidIP (validIPv4s ++ validIPv6s);
+      expected = true;
+    };
+
+    invalidIPv4s = {
+      expr = builtins.any self.lib.strings.isValidIPv4 (invalidIPv4s ++ validIPv6s);
+      expected = false;
+    };
+
+    invalidIPv6s = {
+      expr = builtins.any self.lib.strings.isValidIPv6 (invalidIPv6s ++ validIPv4s);
+      expected = false;
+    };
+
+    invalidIPs = {
+      expr = builtins.any self.lib.strings.isValidIP (invalidIPv4s ++ invalidIPv6s);
+      expected = false;
     };
   };
 }
